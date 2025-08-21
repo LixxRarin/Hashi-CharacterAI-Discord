1d0549bb2c7621e2e07062a97eccdfa124e0bcba
LixxRarin <lixxrarin@gmail.com>
2025-08-21 17:30:49 -0300

    Add global session data to AIManager and remove unused BotMode and WebHook classes

--- commands/ai_manager.py
+++ commands/ai_manager.py
@@ -10,6 +10,9 @@ from discord.ext import commands
 import utils.func as func
 import AI.cai as cai
 
+# Global session data
+session_data: Dict[str, Any] = {}
+
 class AIManager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
